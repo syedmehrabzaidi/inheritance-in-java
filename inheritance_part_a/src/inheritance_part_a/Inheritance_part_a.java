3889d6aff9c64f033693a1a812ad69861aafaf77
Mehrab Zaidi <45766880+syedmehrabzaidi@users.noreply.github.com>
2021-02-19 02:39:54 +0500

    Add files via upload

---
+++ inheritance_part_a/src/inheritance_part_a/Inheritance_part_a.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritance_part_a;
+
+class University{
+
+int a;
+int b;
+void abc(){
+
+
+}
+class Employee extends University{
+int c;
+
+}
+class Student extends University{
+
+
+}
+                            class Teacher extends Employee{
+
+}
+
+                            class Management extends Employee{
+
+}
+                            
+class BS extends Student{}
+class MS extends Student{}
+                            
+                            
+                            
+                            
+                            
+}
+
+
+
+
+public class Inheritance_part_a {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    
+    
+    
+    }
+    
+}
